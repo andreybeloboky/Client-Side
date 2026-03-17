c0db1a94dd006f0f3a3b3f7401ae0d20ebdb3d19
Andrey_Beloboky <andrei.beloboky@yandex.ru>
2026-03-17 22:51:05 +0300

    add buffered and print in try-with-resources

--- src/main/java/org/example/Client.java
+++ src/main/java/org/example/Client.java
@@ -7,9 +7,9 @@ import java.util.Scanner;
 public class Client {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        try (Socket socket = new Socket("localhost", 8080)) {
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        try (Socket socket = new Socket("localhost", 8080);
+             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+             PrintWriter out = new PrintWriter(socket.getOutputStream(), true)) {
             String message = scanner.nextLine();
             out.println(message);
             String response = in.readLine();
