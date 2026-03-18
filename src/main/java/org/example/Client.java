2e3d55d6739fce112209238af17a857a5dc23862
Andrey_Beloboky <andrei.beloboky@yandex.ru>
2026-03-18 17:23:47 +0300

    add socket.connect

--- src/main/java/org/example/Client.java
+++ src/main/java/org/example/Client.java
@@ -1,15 +1,17 @@
 package org.example;
 
 import java.io.*;
+import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        try (Socket socket = new Socket("localhost", 8080);
+        try (Socket socket = new Socket();
              BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
              PrintWriter out = new PrintWriter(socket.getOutputStream(), true)) {
+            socket.connect(new InetSocketAddress("localhost", 8080), 3000);
             String message = scanner.nextLine();
             out.println(message);
             String response = in.readLine();
